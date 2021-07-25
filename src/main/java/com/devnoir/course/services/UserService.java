ea5830867771049b56a729c45f8c899848333523
emelinepg <emelinepg@gmail.com>
2021-07-25 14:56:50 -0300

    Service layer, component registration

---
+++ src/main/java/com/devnoir/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.devnoir.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devnoir.course.entities.User;
+import com.devnoir.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
